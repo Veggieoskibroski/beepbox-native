c5f5a5639a1404909a36a44262e5a6a2ebd794bd
Veg <60745091+Veggieoskibroski@users.noreply.github.com>
2022-06-10 00:23:17 -0500

    Major update. Almost ready for release

--- main.js
+++ main.js
@@ -1,18 +1,56 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const http = require('https'); // or 'https' for https:// URLs
+const fs = require('fs');
+
+function threrr() {
+    const mainWindow = new BrowserWindow({
+        width: 300,
+        height: 150
+    })
+        
+    mainWindow.loadFile('errorHand.html')
+    mainWindow.setMenu(null)
+} 
 
 const createWindow = () => {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
-
-  mainWindow.loadFile('beepbox_offline.html')
-
+    
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+        
+    mainWindow.loadFile('beepbox_offline.html')
+    mainWindow.setMenu(null)
+        
 }
 
 app.whenReady().then(() => {
-  createWindow()
+        const request = http.get("https://www.beepbox.co/beepbox_offline.html", function(response) {
+            const file = fs.createWriteStream(__dirname + "/beepbox_offline.html");
+        response.pipe(file);
+
+       // after download completed close filestream
+       file.on("finish", () => {
+                file.close();
+                console.log("Beepbox Download Complete");
+                createWindow()
+            });
+        }).on("error", (err) => {
+            console.log("Offline!")
+
+            fs.access(__dirname + '/beepbox_offline.html',(err) => {
+                console.log(err)
+                if (err) {
+                    threrr()
+                } else {
+                    createWindow()
+                }
+        
+        })
+
+        });
+  
 
   app.on('activate', () => {
 
